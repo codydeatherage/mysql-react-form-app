970d2be45b697925fce03b5bfe4578744cbcb8bb
Cody Deatherage <cdeatherage@koiconsults.com>
2024-03-21 16:03:52 -0700

    change from useState to useReducer to manage state

--- client/src/components/LabeledInput.jsx
+++ client/src/components/LabeledInput.jsx
@@ -7,11 +7,12 @@ const LabeledInput = ({ id, value, onChange }) => {
     <div style={{ display: "flex", flexDirection: "column" }}>
       <label htmlFor={id}>{labelTitle}</label>
       <input
+        name={id}
         onChange={onChange}
         type={id === "password" ? "password" : "text"}
         id={id}
         value={value}
-      ></input>
+      />
     </div>
   );
 };
--- client/src/components/UserForm.jsx
+++ client/src/components/UserForm.jsx
@@ -1,28 +1,44 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import LabeledInput from "./LabeledInput";
 import axios from "axios";
 
+const initialFormState = {
+  username: "",
+  email: "",
+  password: "",
+};
+
+const reducer = (state, action) => {
+  const { type, field, payload } = action;
+  switch (type) {
+    case "RESET_FORM":
+      return initialFormState;
+    default:
+      return { ...state, [field]: payload };
+  }
+};
+
 const UserForm = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [formState, dispatch] = useReducer(reducer, initialFormState);
 
   const clearForm = () => {
-    setUsername("");
-    setEmail("");
-    setPassword("");
+    dispatch({ type: "RESET_FORM" });
   };
 
   //check that each field is filled
   const validateForm = () => {
-    //can also be written !!(username && email && password)
-    return Boolean(username && email && password);
+    //can also be written !!(formState.username && formState.email && formState.password)
+    return Boolean(formState.username && formState.email && formState.password);
+  };
+
+  const handleChange = (e) => {
+    dispatch({ field: e.target.name, payload: e.target.value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    //same as {username: username, email: email, password: password}
-    const payload = { username, email, password };
+    //same as {username: formState.username, email: formState.email, password: formState.password}
+    const payload = formState;
     const isValid = validateForm();
     if (isValid) {
       try {
@@ -50,18 +66,18 @@ const UserForm = () => {
       >
         <LabeledInput
           id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          value={formState.username}
+          onChange={handleChange}
         />
         <LabeledInput
           id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          value={formState.email}
+          onChange={handleChange}
         />
         <LabeledInput
           id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={formState.password}
+          onChange={handleChange}
         />
         <div style={{ display: "flex" }}>
           <button type="submit" style={{ marginLeft: "auto" }}>
